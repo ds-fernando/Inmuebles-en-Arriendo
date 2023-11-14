6ce1b9e234effc2cb1cd8238cd26d938d4047676
Fernando León <fernandoleonsierra@gmail.com>
2023-11-14 15:36:28 -0500

    limpieza

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ source = "inmuebles.csv"
 
 df = pd.read_csv(source)
 
-
-columnas= list(df.columns)  ##Revisando las columnas
-columnas_mantener =['Codigo SAE', 'Tipo Inmueble', 'Ciudad', 'Departamento', 'Estrato', 'Valor Arriendo', 'Area Construida']
-
-
-df_nuevo = df.drop(columns=[col for col in columnas if col not in columnas_mantener])
-
 #Para empezar la limpieza de los datos se clasifica cuales columnas son categoricas o numericas. 
 # Tipo inmueble = categorica
 # Ciudad = Categorica
@@ -23,6 +16,16 @@ df_nuevo = df.drop(columns=[col for col in columnas if col not in columnas_mante
 #Valor arriendo = numerica
 #codigo sae = numerica
 #estrato = numerica
+
+
+
+
+columnas= list(df.columns)  ##Revisando las columnas
+columnas_mantener =['Tipo Inmueble', 'Ciudad', 'Departamento', 'Estrato', 'Valor Arriendo', 'Area Construida']
+
+
+df_nuevo = df.drop(columns=[col for col in columnas if col not in columnas_mantener])
+
 
 print(df_nuevo.columns)
 
