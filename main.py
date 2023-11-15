3754c8ec287cdc88dc3b18f7ed69e58355797287
Fernando León <fernandoleonsierra@gmail.com>
2023-11-15 08:38:21 -0500

    limpieza

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot
-import matplotlib
+import matplotlib.pyplot as plt
 
 
 source = "inmuebles.csv"
 
 df = pd.read_csv(source)
 
-#Para empezar la limpieza de los datos se clasifica cuales columnas son categoricas o numericas. 
-# Tipo inmueble = categorica
-# Ciudad = Categorica
-#Departamento = categorica
-#Area construida =numerica 
-#Valor arriendo = numerica
-#codigo sae = numerica
-#estrato = numerica
 
+columnas = df.columns
 
-
-
-columnas= list(df.columns)  ##Revisando las columnas
 columnas_mantener =['Tipo Inmueble', 'Ciudad', 'Departamento', 'Estrato', 'Valor Arriendo', 'Area Construida']
-
 
 df_nuevo = df.drop(columns=[col for col in columnas if col not in columnas_mantener])
 
 
-print(df_nuevo.columns)
+#Para empezar la limpieza de los datos se clasifica cuales columnas son categoricas o numericas.   -----> #print(df_nuevo.dtypes)
 
+#QUITAR COMAS Y CAMBIAR EL TIPO DE DATOS DE CADA COLUMNA QUE SE SUPONE QUE ES NUMERICA
+
+df_nuevo['Valor Arriendo'] = df_nuevo['Valor Arriendo'].astype('str')
+df_nuevo['Valor Arriendo'] = df_nuevo['Valor Arriendo'].str.replace(',', '')
+
+df_nuevo['Valor Arriendo'] = df_nuevo['Valor Arriendo'].astype('int64')
+
+columnas_numericas = ['Estrato', 'Valor Arriendo', 'Area Construida']
+for columna in columnas_numericas:
+    df_nuevo[columna] = pd.to_numeric(df_nuevo[columna], errors='coerce')
 
